d09704e86aed6addf577e328c0fdfa14bce7a0c8
Thays <thayscasado@hotmail.com>
2020-09-02 16:52:41 -0700

    Add project files.

---
+++ CsharpFundametals/Math/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpFundametals.Math
+
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ CsharpFundametals/Program.cs
@@ -0,0 +1,20 @@
+﻿using CsharpFundametals.Math;
+
+namespace CsharpFundametals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var results = calculator.Add(1, 2);
+            System.Console.WriteLine(results);
+
+        }
+    }
+}
